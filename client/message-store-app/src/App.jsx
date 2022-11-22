4a58c3a76491c52e1a39690d623bad62de6f337d
kolexstrings <66323820+kolexstrings@users.noreply.github.com>
2022-11-22 13:19:27 +0100

    simple app interface created with vanilla css

--- client/message-store-app/src/App.jsx
+++ client/message-store-app/src/App.jsx
@@ -1,34 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import messageStore from '../../../smart_contract/build/contracts/messageStore.json';
+import { useEffect } from 'react';
+
+const App = () => {
+
+const loadWeb3 = async() => {
+
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    window.alert('Install MetaMask');
+  }
+
+}
+
+const [account, setAccount] = useState('');
+const [messageStore, setMessage] = useState(null);
+const [loading, setLoading] = useState (true);
+
+const loadBlockChain = async() => {
+  const web3 = windows.web3;
+  const account = await web3.eth.getAccounts();
+  setAccount(account[0]);
+
+  const networkId = await web3.eth.net.getId();
+
+  const networkData = messageStore.networks[networkId];
+
+  if (networkData) {
+
+    const messageStore = new web3.eth.Contract (
+      messageStore.abi, networkData.address
+    );
+
+    setMessage();
+    getMessage();
+
+    setLoading(false);
+
+  } else {
+    window.alert('Message Store contract not deployed to that network');
+  }
+}
+
+useEffect(()=> {
+  loadWeb3();
+  loadBlockChain();
+})
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+
+      {/* { loading ? (
+        <p>Loading</p>
+      ) : (
+        <p>Loaded!</p>
+      )}
+      <h1>I am ready</h1> */}
+      <Dapp />
     </div>
-  )
+  );
+}
+
+const Dapp = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="title">
+
+          <div>
+          <h1>Message Store App</h1>
+          <h4>Store Encrypted Messages In One Click</h4>
+          </div>
+
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+
+        </div>
+
+        <div className="main">
+
+          <h4>Current Message:</h4>
+          <p>Make sure you are connected to network</p>
+          <hr></hr>
+
+          <h4>New Message:</h4>
+          <form>
+            <textarea className='input' id="messageInput" placeholder="Type in your message" name="messageInput"/>
+          </form>
+
+          <button className='update-button'>Update</button>
+          
+        </div>
+
+        
+          
+      </div>
+    </div>
+  );
+  
 }
 
 export default App
